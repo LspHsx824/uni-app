a05c7f92ed80f8be96fd3c40759d4f37ac64ee09
cat_mini <1667901324@qq.com>
2022-03-17 12:00:57 +0800

    商品列表页 制作完成

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ uni.$http = $http
 // 请求的根路径
 $http.baseUrl = 'https://www.uinav.com'
 
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- subpack/good_list/good_list.vue
+++ subpack/good_list/good_list.vue
@@ -1,6 +1,11 @@
 <template>
-	<view>
-		商品列表
+	<view class="goods-list">
+		<view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+			<goods-item  :goods="goods">
+				
+			</goods-item>
+		</view>
+		
 	</view>
 </template>
 
@@ -8,12 +13,76 @@
 	export default {
 		data() {
 			return {
-				
+				goodsList:[],
+				queryObj:{
+					query:"",
+					cid:"",
+					pagenum:1,
+					pagesize:10
+				},
+				 // 总数量，用来实现分页
+				total: 0,
+				isLoading:false
 			};
+		},
+		onLoad(options){
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			
+			// 初始化加载 
+			this.getGoodsList()
+		},
+		methods:{
+			async getGoodsList(isPullDown = false){
+				this.isLoading = true;
+				  // 判断是否还有下一页数据
+
+				try{
+					const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+					
+					if( isPullDown){
+						this.goodsList = [...res.message.goods,...this.goodsList,]
+						uni.stopPullDownRefresh()
+					}else{
+						this.goodsList = [...this.goodsList,...res.message.goods]
+					}
+					
+					// console.log(this.goodsList[0].goods_name)
+					this.total = res.message.total
+					this.isLoading = false
+				}catch(err){
+					this.isLoading = false
+					return  uni.$showMsg()
+				}
+			},
+			gotoDetail(item){
+				console.log(item)
+				uni.navigateTo({
+					url:"/subpack/goodes_detail/goodes_detail?goods_id=" + item.goods_id
+				})
+			}
+		},
+		onReachBottom(){
+			/* 判断数据是否加载完毕 */
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+			/* 数据是否在加载中 */
+			if(this.isLoading) return
+			
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh(){
+			/* 判断数据是否加载完毕 */
+			
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+			
+			this.queryObj.pagenum++
+			this.getGoodsList(true)
+			
 		}
 	}
 </script>
 
 <style lang="scss">
-
+ 
 </style>
